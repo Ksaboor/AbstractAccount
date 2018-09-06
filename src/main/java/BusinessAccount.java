79411d5d69f2d4dec1f14782121fd9d771e34359
Khail Saboor <khalilsaboor1@gmail.com>
2018-09-06 15:17:55 -0400

    checking and business account created.

---
+++ src/main/java/BusinessAccount.java
@@ -0,0 +1,5 @@
+public class BusinessAccount extends Account {
+    public BusinessAccount(double intialDeposite) {
+        super(intialDeposite);
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
 
-    public static void main(String[] args){
-        //reading form the keyboard
-        Scanner input = new Scanner(System.in);
-        //request user deposit
-        System.out.print("Please enter initial deposit");
-        double amount = input.nextDouble();
-
-        SavingAccount account = new SavingAccount(amount, 2.0);
-
+    public static void main(String[] args) {
+//        //reading form the keyboard
+//        Scanner input = new Scanner(System.in);
+//        //request user deposit
+//        System.out.print("Please enter initial deposit");
+//        double amount = input.nextDouble();
+//
+//        SavingAccount account = new SavingAccount(amount, 2.0);
 
 
     }

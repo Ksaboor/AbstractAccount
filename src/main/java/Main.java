2edf79c13c0d950a947efc2cfcdad8cd018ebce1
Khail Saboor <khalilsaboor1@gmail.com>
2018-09-07 12:33:15 -0400

    stubbing out a console with a switch statement.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,14 +4,30 @@ import java.util.Stack;
 public class Main {
 
     public static void main(String[] args) {
-//        //reading form the keyboard
-//        Scanner input = new Scanner(System.in);
-//        //request user deposit
-//        System.out.print("Please enter initial deposit");
-//        double amount = input.nextDouble();
-//
-//        SavingAccount account = new SavingAccount(amount, 2.0);
+        //reading form the keyboard
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Enter 1 checking, 2 for savings, and 3 for Business:");
+        int choice = input.nextInt();
+
+        switch (choice){
+            case 1:
+                CheckingAccount ch = new CheckingAccount(70000);
+
+            case 2:
+                //request user deposit
+                System.out.print("Please enter initial deposit");
+                double amount = input.nextDouble();
+
+                SavingAccount account = new SavingAccount(amount, 2.0);
+
+                System.out.println("Enter 1 for deposit, 2 for withdrawal:");
+                choice = input.nextInt();
+
+            case 3:
+                BusinessAccount bs = new BusinessAccount(225000);
+                
+        }
 
     }
 }
